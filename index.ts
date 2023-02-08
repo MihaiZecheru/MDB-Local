488200fce38d30508248cc48f0e3ea268668ffa5
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-02-07 21:49:28 -0800

    Add dotenv

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+require('dotenv').config();
 
 class Database {
   constructor(private readonly path: string, private readonly user: string, private readonly password: string) {}
@@ -6,9 +7,12 @@ class Database {
   connect() {
     // Check if the database exists
     if (!fs.existsSync(this.path)) {
-      console.error('Database does not exist at ' + this.path + "\n\nUse MDBL_setup.exe to create a new database");
+      console.error('Database does not exist at ' + this.path + "\nUse MDBL_setup.exe to create a new database");
     }
+
+    
   }
 }
 
-const db = new Database('C:\\Users\\User\\Documents\\MyDatabase.mdb', 'admin', 'password');
+const db = new Database(<string>process.env.DB_PATH, <string>process.env.USERNAME, <string>process.env.PASSWORD);
+db.connect();
